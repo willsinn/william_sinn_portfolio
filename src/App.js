2cd8915ada5fd468739bdb23dd6a2f308a7c9ac2
William Sinn <w.h.sinn@gmail.com>
2021-11-16 07:29:20 -0500

    add sections for general layout of page

--- src/App.js
+++ src/App.js
@@ -9,7 +9,22 @@ const App = () => {
         Will Sinn
         This is the page's header
       </section>
-      
+
+      <section className="about">
+        About Will Sinn
+      </section>
+
+      <section className="skillset">
+        This is where my skills go
+      </section>
+
+      <section className="projects">
+        Project 1
+        Project 2
+        Project 3
+        Project 4
+      </section>    
+        
     </div>
   );
 }
