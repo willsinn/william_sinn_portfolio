349f498d5673abdbf8a14606a06ade121364f034
William Sinn <w.h.sinn@gmail.com>
2021-11-23 02:41:34 -0500

    finalize positioning and element nesting

--- src/App.js
+++ src/App.js
@@ -8,17 +8,22 @@ const App = () => {
       Navigation
       <Navbar />
       <section className="header">
-        <h1 className="wrapper">
-          <div className="static-txt">Will Sinn</div>
-          Developer
+        <div className="wrapper">
+        <h1>
+          <div className="static-txt">
+            <span className="header-title">
+              Will Sinn
+            </span>
+          </div>
           <ul class="dynamic-txts">
-            <li><span>YouTuber</span></li>
-            <li><span>Designer</span></li>
-            <li><span>Developer</span></li>
-            <li><span>Freelancer</span></li>
+            <li><span className="header-subtitle">YouTuber</span></li>
+            <li><span className="header-subtitle">Designer</span></li>
+            <li><span className="header-subtitle">Developer</span></li>
+            <li><span className="header-subtitle">Freelancer</span></li>
           </ul>
-        </h1>
-        
+          </h1>  
+
+        </div>
       </section>
 
       <section className="about">
