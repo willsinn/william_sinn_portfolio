acbab9b7c5b947b0fbb28aafde526d56194cf752
William Sinn <w.h.sinn@gmail.com>
2021-11-23 07:50:01 -0500

    add state change on MOUSE hover, reminder that there is no hover functionality for mobile

--- src/Components/Skillset.js
+++ src/Components/Skillset.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import { useState } from 'react/cjs/react.development';
 import JS from "../Assets/Icons/js.png";
 
-const Skillset = () => {
 
+const jsAnimations= ()=> {
+    return 
+    <div>hi</div>
+}
+const Skillset = () => {
+    const [active, setActive] = useState("")
+
+    const handleHover = (skill) => {
+        console.log(active)
+        setActive(skill)    
+    }
     return (
         <section className="skillset">
             <div className="skill-tv">
@@ -10,7 +21,7 @@ const Skillset = () => {
                 <span>the skill tv</span>
             </div>
             <ul className="skill-icons">
-                <li className="skill-icon"><span><img className="skill-icon-img" src={JS}/></span></li>
+                <li className="skill-icon"><span><img onMouseOver={(e) => handleHover(e, "js")} className="skill-icon-img" src={JS}/></span></li>
                 <li className="skill-icon"><span><img className="skill-icon-img" src={JS}/></span></li>
                 <li className="skill-icon"><span><img className="skill-icon-img" src={JS}/></span></li>
                 <li className="skill-icon"><span><img className="skill-icon-img" src={JS}/></span></li>
